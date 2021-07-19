b55dff79302c1fc3162daadebad4f3d20a8fc649
Marc319-jr <marcelo.sznek@gmail.com>
2021-07-19 03:41:12 -0300

    fin de validacion de creacion de productos del lado FRONT-END, falta BACK-END con express-validator

--- public/js/validacion-creacionProducto.js
+++ public/js/validacion-creacionProducto.js
@@ -13,7 +13,12 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{6,12}$/, // 6 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    stock: /^\d{1,5}$/,
+    rodados: /^[0-9\,]{1,20}$/,
+    colores: /^[a-zA-Z\,]{1,30}$/,
+    precio: /^[0-9\,]{1,7}$/,
+    descuento: /^[0-9\,]{1,3}$/,
+    cuotas: /^[0-9\,]{1,2}$/
 }
 
 errores = {
@@ -83,7 +88,6 @@ const validatorTextArea = (e) => {
     }
 }
 
-
 //funciones de validacion de inputs
 
 validarMarca = function(e){
@@ -133,8 +137,151 @@ validarModelo = function(e){
     {
         modelo.style.border = "none"
         modelo.style.outline = "2px solid green"   
-        delete errores.marca
-
+        delete errores.modelo
+    }
+}
+validarImagen = function(e){
+    const imagen = document.querySelector(".foto-principal")
+    console.log(e.target.value);
+    if(e.target.value.includes(".jpg")  || e.target.value.includes(".jpeg") || e.target.value.includes(".png"))
+    {
+        console.log("Imagen valida");
+        document.querySelector("#errorImagen").innerHTML = "Imagen valida"
+        delete errores.imagen
+    }
+    else 
+    {
+        console.log("Imagen invalida");
+        document.querySelector("#errorImagen").innerHTML = "La imagen tiene que se de tipo .jpg, .jpeg o .png"
+        errores.imagen = "La imagen tiene que se de tipo .jpg, .jpeg o .png"
+    }
+}
+validarRodados = function(e){
+    rodados = document.querySelector(".rodados")
+    if(rodados.value == "")
+    {
+        rodados.style.border = "none"
+        rodados.style.outline = "1px solid gray"
+        errores.rodado = "El campo de los rodados no puede quedar vacio"
+    }
+    else if(expresiones.rodados.test(rodados.value))
+    {
+        rodados.style.border = "none"
+        rodados.style.outline = "2px solid green"   
+        delete errores.rodado
+    }
+    else
+    {
+        rodados.style.border = "none"
+        rodados.style.outline = "2px solid red"   
+        errores.rodado = "Los rodadps deben ser ingresado separados por coma sin espacisos"
+    }
+}
+validarColores = function(e){
+    colores = document.querySelector(".colores");
+    if(colores.value == "")
+    {
+        colores.style.border = "none"
+        colores.style.outline = "1px solid gray"
+        errores.color = "El campo de los colores no puede quedar vacio"
+    }
+    else if(expresiones.colores.test(colores.value))
+    {
+        colores.style.border = "none"
+        colores.style.outline = "2px solid green"
+        delete errores.color
+    }
+    else
+    {
+        colores.style.border = "none"
+        colores.style.outline = "2px solid red"
+        errores.color = "Los colores deber ser ingresados con letras sin acentos separados por una coma"
+    }
+}
+validarStock = function(e){
+    stock = document.querySelector(".stock")
+    if(stock.value == "")
+    {
+        stock.style.border = "none"
+        stock.style.outline = "1px solid gray"    
+        errores.stock = "El campo stock no puede quedar vacio"    
+    }
+    else if(expresiones.stock.test(stock.value))
+    {
+        stock.style.border = "none"
+        stock.style.outline = "2px solid green"
+        delete errores.stock
+    }
+    else
+    {
+        stock.style.border = "none"
+        stock.style.outline = "2px solid red"
+        errores.stock = "El stock debe ser un numero entre 1 y 99999"
+    }
+}
+validarPrecio = function(e){
+    let precio = document.querySelector(".precio")
+    if(precio == "")
+    {
+        precio.style.border = "none"
+        precio.style.outline = "1px solid gray"
+        errores.precio = "El campo del precio no debe quedar vacio"
+    }
+    else if(expresiones.precio.test(precio.value))
+    {
+        precio.style.border = "none"
+        precio.style.outline = "2px solid green"
+        delete errores.precio
+    }
+    else
+    {
+        precio.style.border = "none"
+        precio.style.outline = "2px solid red"
+        errores.precio = "El precio debe ser ingresado con numeros entre 1 a 9999999"
+    }
+}
+validarDescuento = function(e)
+{
+    let descuento = document.querySelector(".descuento")
+    if(descuento == "")
+    {
+        descuento.style.border = "none"
+        descuento.style.outline = "1px solid gray"
+        errores.descuento = "El campo del descuento no debe quedar vacio"
+    }
+    else if(expresiones.descuento.test(descuento.value) && descuento.value <=100)
+    {
+        descuento.style.border = "none"
+        descuento.style.outline = "2px solid green"
+        delete errores.descuento;
+    }
+    else
+    {
+        descuento.style.border = "none"
+        descuento.style.outline = "2px solid red"
+        errores.descuento = "El campo del descuento debe ser un numero entre 1 y 100"
+    }
+}
+validarCuotas = function(e)
+{
+    let cuotas = document.querySelector(".cuotas")
+    if(cuotas.value == "")
+    {
+        cuotas.style.border = "none"
+        cuotas.style.outline = "1px solid gray"
+        errores.cuotas = "El campo de cuotas no debe quedar vacio"
+    }
+    else if(expresiones.cuotas.test(cuotas.value) && cuotas.value <=12)
+    {
+        cuotas.style.border = "none"
+        cuotas.style.outline = "2px solid green"
+        delete errores.cuotas
+    }
+    else
+    {
+        cuotas.style.border = "none"
+        cuotas.style.outline = "2px solid red"
+        errores.cuotas = "El campo de cuotas debe ser entre 1 y 12 cuotas"
     }
 }
 
@@ -154,31 +301,31 @@ const validarInputs = (e) => {
             break;
         case "imagen":
             console.log("Imagen");
-            //logica de error
+            validarImagen(e);
             break
         case "rodadosProducto":
             console.log("Rodado Prudctos");
-            //logica de error
+            validarRodados(e);
             break
         case "coloresProducto":
             console.log("Colores Producto");
-            //logica de error
+            validarColores(e);
             break
         case "stockProducto":
             console.log("stock Productos");
-            //logica de error
+            validarStock(e)
             break
         case "precioProducto":
             console.log("Precio");
-            //logica de error
+            validarPrecio(e)
             break
         case "descuentoProducto":
             console.log("Descuento");
-            //logica de error
+            validarDescuento(e);
             break
         case "cuotasProducto":
             console.log("Cuotas");
-            //logica de error
+            validarCuotas(e);
             break    
     }
 }
@@ -186,7 +333,7 @@ const validarInputs = (e) => {
 
 inputs.forEach((input) => {
     input.addEventListener("blur" , validarInputs);
-    input.addEventListener("keyup" , validarInputs)
+    input.addEventListener("keyup" , validarInputs);
 })
 
 
